428cdb0b08eeff75b9db658a6b7775533f7b3201
himani460 <himanisengar0@gmail.com>
2021-03-17 21:18:30 +0530

    Commit New Message

--- GitMaven/src/test/java/TestNameGitEclipse.java
+++ GitMaven/src/test/java/TestNameGitEclipse.java
@@ -4,7 +4,7 @@ public class TestNameGitEclipse {
 	public static void main(String[] args) {
 
 
-		System.out.println("Priyanka Sengar");
+		System.out.println("Priyanka Sengar Papa Sengar");
 
 	}
 
