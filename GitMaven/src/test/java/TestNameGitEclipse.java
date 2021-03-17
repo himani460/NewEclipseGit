52e042384103a1ab47a39f91cae20fc83a30b521
himani460 <56784298+himani460@users.noreply.github.com>
2021-03-17 21:24:22 +0530

    Yes

--- GitMaven/src/test/java/TestNameGitEclipse.java
+++ GitMaven/src/test/java/TestNameGitEclipse.java
@@ -4,7 +4,7 @@ public class TestNameGitEclipse {
 	public static void main(String[] args) {
 
 
-		System.out.println("Priyanka Sengar Yogesh Sengar");
+		System.out.println("Priyanka Sengar New Sengar");
 
 	}
 
